6144c232eb2a572aed0eca9397041a66e874ca0a
Steve Richardson <stever@bgs.ac.uk>
2020-10-08 11:14:53 +0100

    ci: updates

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -5,7 +5,7 @@ import { isPlatformBrowser } from '@angular/common';
 export class MapService {
   public L = null;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
     if (isPlatformBrowser(platformId)) {
       this.L = require('leaflet');
     }
